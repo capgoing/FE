c4efe4cd57b0b5ea32f2a15864365a95508808ec
Inseo Yang <113487989+sheepyis@users.noreply.github.com>
2025-03-30 23:04:38 +0900

    ✨ feat: qnaModal, resultPage UI 개발 (#7)

--- src/styles/common/colors.js
+++ src/styles/common/colors.js
@@ -2,8 +2,13 @@ const colors = {
     mainYellow: "#FFD839",
     green: "#00B718",
     red: "#FF3838",
+    orange: "#FF9400",
+    orange2: "#FE8F00",
+    brown: "#7D4D00",
     gray: "#D9D9D9",
     gray2: "#F2F3F2",
+    gray3: "#888686",
+    gray4: "#484A64",
     black: "#000000",
     white: "#FFFFFF",
 };
