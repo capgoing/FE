b16b308de43222bf2ea6e72f504bf4bd9d266e33
binniejung <ybe1027@naver.com>
2025-04-07 14:36:21 +0900

    🐛 Fix: 머지 과정에서 누락된 그래프 페이지 코드 복구

--- src/pages/graph/graph.jsx
+++ src/pages/graph/graph.jsx
@@ -1,18 +1,14 @@
-import styled from "styled-components";
-import colors from "../../styles/common/colors";
-
-const GraphContainer = styled.div`
-  width: 76vw;
-  height: 43.4vw;
-  border-radius: 2vw;
-  background: ${colors.white};
-  margin-top: 1.1vw;
-`;
+import GraphButton from "../../components/graph/graphButton";
+import * as G from "../../styles/graph/graph";
+import GraphFlow from "../../components/graph/graphFlow";
 
 const Graph = () => {
   return (
     <div className="pageContainer">
-      <GraphContainer></GraphContainer>
+      <G.GraphContainer>
+        <GraphFlow />
+        <GraphButton />
+      </G.GraphContainer>
     </div>
   );
 };
