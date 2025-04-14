04b644ca3a938d6c7f30df0df56ffb36a1a61ead
양인서 <113487989+sheepyis@users.noreply.github.com>
2025-04-14 16:08:42 +0900

    ✨ feat: graph 페이지 노드 삭제 구현 완료

--- src/components/graph/graphMenu.jsx
+++ src/components/graph/graphMenu.jsx
@@ -2,12 +2,23 @@ import * as G from "../../styles/graph/graph";
 import Close from "../../assets/images/header/close.png";
 import { useState } from "react";
 import GraphNodeAdd from "./graphNodeAdd";
-
+import { API } from "../../apis/axios";
 
 
 const GraphMenu = ({ node, onClose }) => {
     const { id, data } = node;
     const [isAddMode, setIsAddMode] = useState(false);
+
+    const handleDelete = async () => {
+        try {
+          const response = await API.delete(`/users/${id}`);
+          alert("노드가 삭제되었습니다.");
+          console.log(response.data);
+          onClose();
+        } catch (err) {
+          console.error("노드 삭제 실패:", err);
+        }
+      };
 
     return (
         <G.MenuContainer>
@@ -20,7 +31,7 @@ const GraphMenu = ({ node, onClose }) => {
             ) : (
                 <G.MenuList>
                     <G.MenuItem onClick={() => setIsAddMode(true)}>노드 추가</G.MenuItem>
-                    <G.MenuItem>노드 삭제</G.MenuItem>
+                    <G.MenuItem onClick={handleDelete}>노드 삭제</G.MenuItem>
                 </G.MenuList>
             )}
         </G.MenuContainer>
