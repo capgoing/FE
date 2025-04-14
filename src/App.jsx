f992fde9099d5b7cdfe0cffd46898ad2d9e1ddd2
Inseo Yang <113487989+sheepyis@users.noreply.github.com>
2025-04-14 12:11:41 +0900

    🐛 fix: 그래프페이지 다크모드 오류 수정 완료 (#15)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import GlobalStyle from "./styles/common/globalStyles";
 import router from "./routes/router";
@@ -6,8 +7,40 @@ import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./styles/common/theme";
 
 function App() {
-  const  { isEditMode } = useEditMode();
+  const { isEditMode, setIsEditMode } = useEditMode();
   const theme = isEditMode ? darkTheme : lightTheme;
+
+  useEffect(() => {
+    const checkAndUpdateEditMode = () => {
+      const path = window.location.pathname;
+      const isGraphPage = path.startsWith("/graph");
+      if (!isGraphPage) {
+        localStorage.setItem("editMode", "false");
+        setIsEditMode(false);
+      }
+    };
+
+    checkAndUpdateEditMode();
+
+    const patchHistory = (method) => {
+      const original = history[method];
+      return function (...args) {
+        const result = original.apply(this, args);
+        window.dispatchEvent(new Event("locationchange"));
+        return result;
+      };
+    };
+    history.pushState = patchHistory("pushState");
+    history.replaceState = patchHistory("replaceState");
+
+    window.addEventListener("popstate", checkAndUpdateEditMode);
+    window.addEventListener("locationchange", checkAndUpdateEditMode);
+
+    return () => {
+      window.removeEventListener("popstate", checkAndUpdateEditMode);
+      window.removeEventListener("locationchange", checkAndUpdateEditMode);
+    };
+  }, [setIsEditMode]);
 
   return (
     <ThemeProvider theme={theme}>
