4d513a2c5519aa3c7531bdd2c5a1bc2448e0fc7a
yebin <122111459+benniejung@users.noreply.github.com>
2025-04-16 19:48:28 +0900

    ✨ Feat: 그래프 페이지-챗봇 UI 및 슬라이드 애니메이션 구현 (#23)
    * ✨ Feat: 그래프 페이지-챗봇 UI 및 슬라이드 애니메이션 구현
    
    * ✨ Feat: 수정모드일 때는 챗봇 사라지도록 코드 추가

--- src/pages/graph/graph.jsx
+++ src/pages/graph/graph.jsx
@@ -2,15 +2,22 @@ import GraphButton from "../../components/graph/graphButton";
 import * as G from "../../styles/graph/graph";
 import GraphFlow from "../../components/graph/graphFlow";
 import { useEditMode } from "../../contexts/editModeContext";
+import { useState } from "react";
 
 const Graph = () => {
   const { isEditMode } = useEditMode();
+  const [isClickChatbotBtn, setIsClickChatbotBtn] = useState(false);
 
   return (
     <div className="pageContainer">
       <G.GraphContainer>
-        <GraphFlow />
-        {!isEditMode && <GraphButton />}
+        <GraphFlow
+          isClickChatbotBtn={isClickChatbotBtn}
+          setIsClickChatbotBtn={setIsClickChatbotBtn}
+        />
+        {!isEditMode && !isClickChatbotBtn && (
+          <GraphButton setIsClickChatbotBtn={setIsClickChatbotBtn} />
+        )}
       </G.GraphContainer>
     </div>
   );
