5e5f49b0b4ab3a38f2f24f4470c5e9b147fe32ce
binniejung <ybe1027@naver.com>
2025-05-27 01:03:03 +0900

    ✨ Feat: 리스트 페이지에 Loading 컴포넌트 추가

--- src/pages/list/list.jsx
+++ src/pages/list/list.jsx
@@ -2,24 +2,30 @@ import * as s from "../../styles/list/list";
 import useGet from "../../hooks/useGet";
 import NotList from "../../components/list/notList/notList";
 import ListWork from "../../components/list/yesList/list-work";
-
+import Loading from "../../components/quiz/qna/Loading";
 
 const List = () => {
-    const { data } = useGet("/graph");
-    // console.log(data);
-    const graphList = data?.data?.graph;
+  const { data, loading } = useGet("/graph");
+  // console.log(data);
+  const graphList = data?.data?.graph;
 
-    return (
-        <div className="pageContainer">
-            <s.ListContainer>
-                {Array.isArray(graphList) && graphList.length > 0 ? (
-                    <ListWork data={graphList} />
-                    ) : (
-                    <NotList />
-                )}
-            </s.ListContainer>
-        </div>
-    )
-}
+  return (
+    <div className="pageContainer">
+      {loading ? (
+        <s.PageContainer>
+          <Loading />
+        </s.PageContainer>
+      ) : (
+        <s.ListContainer>
+          {Array.isArray(graphList) && graphList.length > 0 ? (
+            <ListWork data={graphList} />
+          ) : (
+            <NotList />
+          )}
+        </s.ListContainer>
+      )}
+    </div>
+  );
+};
 
 export default List;
